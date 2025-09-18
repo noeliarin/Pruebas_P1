b16f7232bd9092364c2b501b2e9df3407b4e0c20
noeliarin <noelia.rincon@estudiante.uam.es>
2025-09-19 00:43:25 +0200

    apartado3 medio medio

--- .metadata/.plugins/org.eclipse.core.resources/.history/af/70e8cf0d9f9200101994fb8c0793fe1b
+++
@@ -1,5 +0,0 @@
-package calc.test;
-
-public class DivisionTest {
-
-}
--- SumaListasDigitos/lib/src/main/java/suma/listas/digitos/SumaListasDigitos.java
+++ SumaListasDigitos/lib/src/main/java/suma/listas/digitos/SumaListasDigitos.java
@@ -6,8 +6,6 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 
-
-
 public class SumaListasDigitos {
 
     /**
